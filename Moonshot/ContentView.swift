c25783621c7bb30cc4ff596e32673ebc23ad978e
Amber Spadafora <ahelenespadafora@gmail.com>
2020-10-27 21:25:04 -0400

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Amber Spadafora on 10/27/20.
+//  Copyright © 2020 Amber Spadafora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
